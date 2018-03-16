530daeb6318b934054ff93534994ce915586dfdd
malik303 <a.malicky1@gmail.com>
2018-03-17 02:16:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/adam_malik/orlando/Booking.java
@@ -0,0 +1,16 @@
+package com.example.adam_malik.orlando;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+
+        
+    }
+}
